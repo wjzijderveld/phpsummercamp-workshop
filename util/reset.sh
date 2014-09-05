aad5e442a1b321285943308e0858b7758d795051
Willem-Jan <wjzijderveld@gmail.com>
2014-09-05 15:11:21 +0200

    Fixing stuff during the workshop :D

--- util/reset.sh
+++ util/reset.sh
@@ -42,7 +42,7 @@ function create_workspace {
 function load_cnd()
 {
     CND=$1
-    phpcrsh -t jackrabbit --command "node-type:load --update $DIR/{$CND}.cnd" --command "session:save"
+    phpcrsh -t jackrabbit --command "node-type:load --update $DIR/${CND}.cnd" --command "session:save"
 }
 
 function reset_workspace {
@@ -78,6 +78,14 @@ function reset_workspace {
         echo "Importing fixture data"
         phpcrsh -t jackrabbit --phpcr-workspace=$PART --command "session:import-xml / '$DIR/export-$PART.xml'" --command "session:save"
     fi
+
+    fixtures=(cms routes menu)
+    for fixture in ${fixtures[@]}; do
+        echo "Importing: $DIR/export-$PART-$fixture.xml"
+        if [ -f "$DIR/export-$PART-$fixture.xml" ]; then
+            phpcrsh -t jackrabbit --phpcr-workspace=$PART --command "session:import-xml / '$DIR/export-$PART-$fixture.xml'" --command "session:save"
+        fi
+    done
 }
 
 if [ -z $PART ]; then
